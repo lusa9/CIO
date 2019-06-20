a79e87e6809ee572c6970ea637b9233c54d8dcae
Luka Ušalj <luka.usalj@gmail.com>
2019-06-20 15:40:24 +0200

    added logo

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import logo from '../assets/logo.svg'
 
 const buttons = [
     "About Us",
@@ -10,19 +11,21 @@ const buttons = [
 ]
 
 const Button = (name) => (
-    <div className="h-full white d-flex align-items-center">
-        <div>{name}</div>
+    <div className="h-full d-flex align-items-center">
+        <h4 className="white">{name}</h4>
     </div>
 )
 
 
 export const Header = () => (
-    <div className="h-100 bg-accent content-container">
-        <div className="content d-flex justify-content-between">
-            <div style={{width: 100, backgroundColor: "white"}}/>
+    <div className="h-100 d-flex">
+        <div className="bg-white h-100" style={{display: "flex", flex: 1}}/>
+        <div className="content bg-accent d-flex justify-content-between">
+            <img className="bg-white" src={logo} style={{paddingRight: 40}}/>
             <div className="d-flex">
                 {buttons.map(Button).withSpace({width: 40})}
             </div>
         </div>
+        <div className="bg-accent h-100" style={{display: "flex", flex: 1}}/>
     </div>
 )

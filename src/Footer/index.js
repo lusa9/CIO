8cc93543d179ce0f62366bc9203d3d09ae83e23d
Luka Ušalj <luka.usalj@gmail.com>
2019-06-20 16:31:42 +0200

    implemented footer

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 export const Footer = () => (
-    <div className="h-60 content-container bg-accent"> 
+    <div className="h-60 content-container bg-accent">
+        <div className="content d-flex justify-content-between align-items-center">
+            <h6 className="white">© 2019 The Global CIO Office, Inc. All rights reserved.</h6>
+            <h6 className="white">Legal Notice</h6>
+        </div>
     </div>
 )

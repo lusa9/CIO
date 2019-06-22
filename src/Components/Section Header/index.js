300939fdc43a54a419bbc1a468a74f1d417d83a3
Luka Ušalj <luka.usalj@gmail.com>
2019-06-22 17:11:35 +0200

    updated gray

--- src/Components/Section Header/index.js
+++ src/Components/Section Header/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const SectionHeader = ({name}) => (
-    <div className="w-full h-200 d-flex justify-content-center align-items-center bg-light">
+    <div className="w-full h-200 d-flex justify-content-center align-items-center bg-lightgray">
         <h2 className="bold">{name}</h2>
     </div>
 )

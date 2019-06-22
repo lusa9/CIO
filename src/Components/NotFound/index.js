c766f5af8aff8d0d3a05bcdc7fef9ed58713cf1c
Luka Ušalj <luka.usalj@gmail.com>
2019-06-22 15:07:24 +0200

    implemented coming soon

--- src/Components/NotFound/index.js
+++ src/Components/NotFound/index.js
@@ -4,7 +4,8 @@ import logo from '../../assets/logo.svg'
 
 export const NotFound = () => (
     <div className="d-flex flex-1 content-container">
-        <div className="content debug">
+        <div className="content margin-top-120">
+            <h2 className="accent">Coming Soon...</h2>
         </div>
     </div>
 )

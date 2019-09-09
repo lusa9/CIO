04a09f53f4670b4ef5865e01dd932d70af4c49fa
Luka Ušalj <lusa@brain-it.hr>
2019-09-09 23:24:03 +0200

    added johan

--- src/Components/Team/index.js
+++ src/Components/Team/index.js
@@ -7,12 +7,18 @@ import sam from '../../assets/sam.jpg'
 import aaron from '../../assets/aaron.jpg'
 import luka from '../../assets/luka.jpg'
 import javed from '../../assets/javed.jpg'
+import johan from '../../assets/johan.jpg'
 
 const members = [
     {
         image: gary,
         title: "Gary Dugan - CIO",
         description: "Investment professional with 35+ years experience of buy and sell side of the banking businesses. I have managed numerous strongly performing investment and advisory teams in large banks in Europe and more recently in Asia and the Middle East. I have built businesses from scratch and successfully restructured businesses. I have had links with Asia and particularly Singapore since 1999 when I managed money for the GIC, Hong Kong Jockey Club and Brunei government. The core of my career was the 11 years at JPMorgan where I made Managing Director. On the buy side I have worked for asset management firms (e.g. JPMorgan) and private banks (Barclays, Merrill Lynch, Coutts, Emirates NBD and NBAD). I have a reputation for making bold calls on the markets. I made major bear calls on equity markets in 1987, at the top of the tech boom and the financial crisis in August 2007, and a bull call on equity markets on March 17th 2009. I have been a high profile with the media regularly appearing in the printed press and on TV. I often speak at conferences and provide industry thought leadership on a number of macro and investment topics.  In the past I made a presentation to the European Parliament on the outlook for asset markets after the introduction of the euro. I am highly regarded for my team leadership, the rigour and intellect I bring to the investment process, my attention to governance and ethics, and my affinity with clients in presentations and meetings. I was voted Private Banker of the year 2015 (MENA) from Wealth Briefing."
+    },
+    {
+        image: johan,
+        title: "Johan Jooste - Managing Director",
+        description: "Johan started his career in banking as a currency and interest rates analyst in Johannesburg in 1994. After stints trading currency and managing South African bond portfolio’s, he relocated to the UK in 2001, managing global fixed income portfolios for institutional clients before switching course and joining Merrill Lynch Private Wealth Management in 2008. In this role, which was subsumed into Julius Baer in 2014, Johan was successively entrusted with Fixed Income management (Merrill Lynch) and thereafter overall strategy as CIO: UK for the Julius Baer business. In 2016 Johan relocated to Singapore to take up the role of CIO at Bank of Singapore, before joining Purple Asset Management in 2019 as Managing Director."
     },
     {
         image: bill,

11df87488f2bace27e222f1a805d94314c2d2d33
Luka Ušalj <lusa@brain-it.hr>
2019-09-09 23:18:47 +0200

    added pdfs

--- src/Components/Resources/index.js
+++ src/Components/Resources/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { SectionHeader } from '../Section Header';
 import { Title } from '../Title';
 import resources from '../../assets/resources.jpg'
+
 import TradeWarStrategies from "./TradeWarStrategies.pdf"
+import LessonsForTheEurozoneFromArgentina from "./LessonsForTheEurozoneFromArgentina.pdf"
+import JeNeRegretRien from './JeNeRegretRien.pdf'
+import ItIsLookingMoreLikeJapan from './ItIsLookingMoreLikeJapan.pdf'
 
 export const Resources = () => (
     <div className="d-flex flex-1 flex-direction-column">
         <img src={resources} style={{objectFit: "cover"}}/>
         <div className="content-container margin-top-120 margin-bottom-200">
-            <div className="content flex-1 flex-direction-column">
-                <a href={TradeWarStrategies} download="CIO weekly - Trade War Strategies.pdf">CIO weekly - Trade War Strategies.pdf</a>
+            <div className="content d-flex flex-1 flex-direction-column">
+                <a className="margin-top-10" href={TradeWarStrategies} download="CIO weekly - Trade War Strategies.pdf">CIO weekly - Trade War Strategies</a>
+                <a className="margin-top-10" href={LessonsForTheEurozoneFromArgentina} download="CIO Weekly - Lessons for the eurozone from Argentina.pdf">CIO Weekly - Lessons for the eurozone from Argentina</a>
+                <a className="margin-top-10" href={JeNeRegretRien} download="CIO Weekly - Je ne regret rien.pdf">CIO Weekly - Je ne regret rien</a>
+                <a className="margin-top-10" href={ItIsLookingMoreLikeJapan} download="CIO Weekly - It is Looking More Like Japan.pdf">CIO Weekly - It is Looking More Like Japan</a>
             </div>
         </div>
     </div>

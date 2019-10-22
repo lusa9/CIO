c5482cfc254de112a1dd1fe8bc1a822f89818b97
Luka Ušalj <lusa@brain-it.hr>
2019-10-22 10:17:03 +0200

    updated ocio page

--- src/Components/OCIO/index.js
+++ src/Components/OCIO/index.js
@@ -7,17 +7,19 @@ export const OCIO = () => (
         <SectionHeader name="OCIO"/>
         <div className="content-container margin-top-120 margin-bottom-200">
             <div className="content flex-1 flex-direction-column">
-                <Title text="Why choose The Global CIO Office for your OCIO?"/>
-                <h4 className="margin-top-40 text-justify">Our OCIO teams extensive investment expertise, robust research platform, and advanced technology and analytics are aligned in one single goal: helping clients achieve their investment objectives.</h4>
-                <br/>
-                <h4 className="text-justify">We provide global institutional quality support to your investment office:</h4>
-                <h4 className="text-justify">•  Institutional quality global asset management</h4>
-                <h4 className="text-justify">•  Fiduciary services</h4>
-                <h4 className="text-justify">•  Unwavering objectivity</h4>
-                <br/>
-                <h4 className="text-justify">We sit on your side of the table.</h4>
-                <Title className="margin-top-120" text="A US phenomena now going global"/>
-                <h4 className="margin-top-40 text-justify">“The OCIO market is booming – and its influence is reverberating across the asset management industry.” Alicia McElhaney January 31, 2019 (Institutional Investor)</h4>
+                <h3 className="bold">Why choose The Global CIO Office for your OCIO?"</h3>
+                <h4 className="margin-top-30 text-justify">
+                    Our OCIO teams extensive investment expertise, robust research platform, and advanced technology and analytics are aligned in one single goal: helping clients achieve their investment objectives.<br/>
+                    <br/>
+                    We provide global institutional quality support to your investment office:<br/>
+                    •  Institutional quality global asset management<br/>
+                    •  Fiduciary services<br/>
+                    •  Unwavering objectivity<br/>
+                    <br/>
+                    We sit on your side of the table.
+                </h4>
+                <h3 className="bold margin-top-100">A US phenomena now going global"</h3>
+                <h4 className="margin-top-30 text-justify">“The OCIO market is booming – and its influence is reverberating across the asset management industry.” Alicia McElhaney January 31, 2019 (Institutional Investor)</h4>
             </div>
         </div>
     </div>

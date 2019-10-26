04293d16a3873b3d2a003e61a6a38f448cd3c99a
Luka Ušalj <lusa@brain-it.hr>
2019-10-26 19:10:28 +0200

    updated contacts page

--- src/Components/Section Header/index.js
+++ src/Components/Section Header/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 export const SectionHeader = ({name}) => (
     <div className="content-container margin-top-120">
-        <div className="content">
-            <h1 className="accent">{name}</h1>
+        <div className="content d-flex justify-content-center">
+            <h2>{name}</h2>
         </div>
     </div>
 )
 
 export const PrevioustSectionHeader = ({name}) => (
     <div className="w-full margin-top-100 d-flex align-items-center " >
-        <h1 className="accent">{name}</h1>
+        <h2>{name}</h2>
     </div>
 )
